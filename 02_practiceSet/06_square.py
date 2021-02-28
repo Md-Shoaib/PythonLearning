adf7fea9b467acbc1e06cc99850845e53cf03e1c
Md Shoaib <shoaib10oct@gmail.com>
2021-02-28 11:33:27 +0000

    chapter 02 practice set

---
+++ 02_practiceSet/06_square.py
@@ -0,0 +1,10 @@
+# Author: Md Shoaib
+# Date: 28/02/2021
+# WAP to calculate square of a number entered by the user
+
+num = input("Enter a number you want to square: ")
+num = int(num)
+ 
+num = num*num
+
+print("Square of your number is: ", num)
